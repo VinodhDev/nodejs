52056c441ead2861ee77a42184382a4f67564a90
VinodhDev <135949858+VinodhDev@users.noreply.github.com>
2024-02-22 12:17:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 // Create a server object:
 http.createServer(function (req, res) {
-  res.write('Automate check Now !!!  '); // Write a response to the client
+  res.write('Automate pls check Now !!!  '); // Write a response to the client
   res.end(); // End the response
 }).listen(80); // The server object listens on port 80
